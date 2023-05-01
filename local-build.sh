1344e0d606ba490f72eb49d6f664e5971631a244
Pavel Pivovarov <snowmean@gmail.com>
2023-05-01 19:47:26 +1000

    Reverted YAML config back to JSON

--- local-build.sh
+++ local-build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-rm -f ru.sview.sView.flatpak
 rm -rf build-dir ; mkdir build-dir
 
-flatpak-builder --ccache --force-clean build-dir ru.sview.sView.yaml
-flatpak-builder --user --install --force-clean build-dir ru.sview.sView.yaml
-# flatpak build-bundle _repo ru.sview.sView.flatpak ru.sview.sView master
+flatpak-builder --ccache --force-clean build-dir ru.sview.sView.json
+flatpak-builder --user --install --force-clean build-dir ru.sview.sView.json
