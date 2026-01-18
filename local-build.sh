00ce098f8c7115909796f8c2150eeac175b5861a
Bastien Nocera <hadess@hadess.net>
2026-01-18 18:55:34 +0100

    Update build script to use test branch

--- local-build.sh
+++ local-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+BRANCH=test
+
 rm -f ru.sview.sView.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --ccache --force-clean _build ru.sview.sView.json --repo=_repo
-flatpak build-bundle _repo ru.sview.sView.flatpak ru.sview.sView master
+flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build ru.sview.sView.json --repo=_repo
